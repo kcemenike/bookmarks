6f30c7e1dfc56520a41767fa4b6b6f0486fcb9e7
kcemenike <29731161+kcemenike@users.noreply.github.com>
2020-07-17 12:03:49 +0100

    3. Add account edit. Include flash messages

--- .history/account/forms_20200717094632.py
+++ .history/account/forms_20200717094632.py
@@ -1,12 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
-
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
@@ -21,15 +18,3 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match')
         return cd['password2']
-
-
-class UserEditForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-
-class ProfileEditForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('date_of_birth', 'photo')
--- .history/account/views_20200717095833.py
+++ .history/account/views_20200717095833.py
@@ -2,20 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from .forms import UserRegistrationForm
 
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .forms import LoginForm
 # Create your views here.
-
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd['username'], password=cd['password'])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
 
             if user is not None:
                 if user.is_active:
@@ -29,7 +26,6 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
-
 
 @login_required
 def dashboard(request):
@@ -46,8 +42,6 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save
             new_user.save()
-            # create new user profile
-            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
@@ -60,28 +54,4 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
-    )
-
-
-@login_required
-def edit(request):
-    if request.method == "POST":
-        user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile, data=request.POST, files=request.FILES)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Error updating your profile')
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
-
-    return render(
-        request,
-        'account/edit.html',
-        {'user_form': user_form, 'profile_form': profile_form}
     )
