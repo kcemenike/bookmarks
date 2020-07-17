14152a7913631ede8e8942e0d77b633112683c49
kcemenike <29731161+kcemenike@users.noreply.github.com>
2020-07-17 23:45:26 +0100

    8. Add like to image using Javascript

--- images/views.py
+++ images/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -39,3 +42,21 @@ def image_detail(request, id, slug):
     return render(request,
                   'images/image/detail.html',
                   {'section': 'image', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
